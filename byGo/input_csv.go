e5bd96ebf636632b4a65e950c3665b8f6f874364
kazuku higashiguchi <higashiguchi_kazuki@yahoo.co.jp>
2017-07-22 15:41:36 +0900

    CSVファイルを読み込みデータベースにINSERTする。

--- byGo/input_csv.go
+++ byGo/input_csv.go
@@ -1,32 +1,52 @@
 package main
 
 import(
-  "fmt"
+  // "fmt"
   "encoding/csv"
   "io"
   "os"
+  "strings"
   "database/sql"
+  "log"
   _ "github.com/go-sql-driver/mysql"
 )
 
 func main(){
+  /**
+  * CSVファイル読み込み
+  **/
   var fp *os.File
-  var err error
-
-  if len(os.Args) < 2{
+  if len(os.Args) < 2 {
     fp = os.Stdin
-  }else{
-    fmt.Printf(">> read file: %s\n", os.Args[1])
+  } else {
+    var err error
     fp, err = os.Open(os.Args[1])
-    if err != nil{
-      panic(err)
+    if err != nil {
+            panic(err)
     }
     defer fp.Close()
   }
 
+  /**
+  * DB接続
+  **/
+  dbconf := "root:verysecret@tcp(mysqld:13306)/customer"
+  db, err := sql.Open("mysql", dbconf)
+  if err != nil{
+    log.Fatal("open error: ", err)
+  }
+  defer db.Close()
+
+  /**
+  * DB書き込み
+  **/
+
   reader := csv.NewReader(fp)
   reader.Comma = '\t'
   reader.LazyQuotes = true
+
+  stmtIns, err := db.Prepare("INSERT INTO customers(name, sex, tel) values(?,?,?)")
+
   for{
     record, err := reader.Read()
     if err == io.EOF{
@@ -34,6 +54,21 @@ func main(){
     }else if err != nil{
       panic(err)
     }
-    fmt.Println(record)
+    arr := strings.Split(record[0], " ")
+
+    // DB書き込み
+    name := arr[1]
+    var sex int
+    if arr[2] == "男"{
+      sex = 0
+    }else if arr[2] == "女"{
+      sex = 1
+    }
+    tel := arr[3]
+
+    _, err = stmtIns.Exec(name, sex, tel)
+    if err != nil{
+      log.Fatal("insert error: ", err)
+    }
   }
 }
